e5dce094e1539f1704b7e183310c6bfb4beb747e
Suvadeep Hajra <suvadeep.hajra@gmail.com>
2017-06-27 18:39:43 +0530

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from DataParser import DataParser
-from model import Model1_batch as Model
+from model2 import Model2 as Model
 
 
 # In[ ]:
@@ -7,23 +7,15 @@ from model import Model1_batch as Model
 maxParagraphLength=250
 maxParagraphs=10
 labels=1000
-vocabularySize=300000
+vocabularySize=15000
 model = Model(maxParagraphLength,maxParagraphs,labels,vocabularySize)
 training = DataParser(maxParagraphLength,maxParagraphs,labels,vocabularySize)
-training.getDataFromfile("data/vocab_3L_l1000_red_train.txt")
+training.getDataFromfile("data/vocab_3L_l1000_sampled_10000_red_train.txt")
 
-#testing = DataParser(maxParagraphLength,maxParagraphs,labels,vocabularySize)
-#testing.getDataFromfile("data/vocab_3L_l1000_red_test.txt")
-#model.load("./model1_W3L_l1000_6")
-model.load("./model1_W3L_Alpha_5_batch_50_Exp2_l1000_75")
-print ("-------------Training Begins-------------")
-epoch=76
-epochEnd=150
 batchSize=50
 
-f=open("trainingCost.log","w")
-f.close()
-
+epoch=0
+epochEnd=10
 for e in range(epoch,epochEnd):
     print 'Epoch: ' + str(e)
     cost=0
@@ -31,10 +23,6 @@ for e in range(epoch,epochEnd):
         cost+=model.train(training.nextBatch(batchSize))
         #break
     print (str(cost))
-    print("Epoch ",e," Cost = ",cost)
-    f=open("trainingCost.log","a")
-    f.write("Epoch "+str(e)+" Cost = "+str(cost)+"\n")
-    f.close()
 
     if e % 10 ==0:
-        model.save("model1_W3L_Alpha_5_batch_50_Exp2_l1000_"+str(e))
+        model.save("model2_l1000_"+str(e))
